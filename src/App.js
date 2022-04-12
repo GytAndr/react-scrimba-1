46d8a7b5a49633bc54f1c761f90d654868a56cf7
GytAndr <andriejunas@gmail.com>
2022-04-12 09:11:50 +0300

    boiler

--- src/App.js
+++ src/App.js
@@ -1,49 +1,11 @@
-import React, { Component } from "react";
-import uniqid from "uniqid";
-import Overview from "./components/Overview";
+import Navbar from "./components/Navbar";
+import Main from "./components/Main";
 
-class App extends Component {
-	constructor() {
-		super();
-		this.state = {
-			task: { text: "", id: uniqid() },
-			tasks: [],
-		};
-	}
-
-	handleChange = (e) => {
-		this.setState({
-			task: {
-				text: e.target.value,
-				id: this.state.task.id,
-			},
-		});
-	};
-	onSubmitTask = (e) => {
-		e.preventDefault();
-		this.setState({
-			tasks: this.state.tasks.concat(this.state.task),
-			task: { text: "", id: uniqid() },
-		});
-	};
-	render() {
-		const { task, tasks } = this.state;
-
-		return (
-			<div>
-				<form onSubmit={this.onSubmitTask}>
-					<label htmlFor="taskInput">Enter task</label>
-					<input
-						onChange={this.handleChange}
-						value={task.text}
-						type="text"
-						id="taskInput"
-					/>
-					<button type="submit">Add Task</button>
-				</form>
-				<Overview tasks={tasks} />
-			</div>
-		);
-	}
+export default function App() {
+	return (
+		<div className="container">
+			<Navbar />
+			<Main />
+		</div>
+	);
 }
-export default App;
